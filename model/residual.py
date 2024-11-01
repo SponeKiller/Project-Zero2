c87a345c2a4039ad7f8121e4f6a57136569c1ac0
SponeKiller <124689362+SponeKiller@users.noreply.github.com>
2024-11-01 08:51:45 +0100

    created residual connection

---
+++ model/residual.py
@@ -0,0 +1,46 @@
+from torch import nn
+import torch
+from layer_norm import LayerNorm
+
+class ResidualConnection(nn.Module):
+    
+    """
+        Residual connection
+    """
+    
+    def __init__(self, 
+                 d_model: int, 
+                 dropout: float = 0.3) -> None:
+    
+        """
+        Initializes residual connection 
+        
+        Args:
+            d_model (int): Dimension of the model
+            dropout (float): Dropout rate 
+        
+            
+        """
+            
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNorm(d_model)
+        self.d_model = d_model
+
+    def forward(self, 
+                x: torch.Tensor, 
+                sublayer: nn.Module) -> torch.Tensor:
+        
+        """
+        Forward pass of residual connection
+        
+        Args:
+            x (torch.Tensor): Input tensor
+            sublayer (nn.Module): Sublayer
+        returns:
+            torch.Tensor: Output tensor
+        """
+        assert (x.shape[-1] == self.d_model, 
+                f"Input dim should be {self.d_model}, but given {x.shape[-1]}")
+        
+        return x + sublayer(self.dropout(self.norm(x)))
