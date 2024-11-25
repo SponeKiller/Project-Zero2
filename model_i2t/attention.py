675b7a8a55be4fa59ff1da6280b88a06ad1a5375
SponeKiller <124689362+SponeKiller@users.noreply.github.com>
2024-11-25 08:53:41 +0100

    explicit int
    setting explicit int because of error float cannon be used in view method

--- model_i2t/attention.py
+++ model_i2t/attention.py
@@ -99,9 +99,9 @@ class MultiHeadAttention(nn.Module):
         value = self.v(x)
         
         # (batch_size, seq_len, d_model) -> (batch_size, num_heads, seq_len, d_k)
-        query = query.view(query.size(0), query.size(1), self.num_heads, self.d_k).transpose(1, 2)
-        key = key.view(key.size(0), key.size(1), self.num_heads, self.d_k).transpose(1, 2)
-        value = value.view(value.size(0), value.size(1), self.num_heads, self.d_k).transpose(1, 2)
+        query = query.view(query.size(0), query.size(1), int(self.num_heads), int(self.d_k)).transpose(1, 2)
+        key = key.view(key.size(0), key.size(1), int(self.num_heads), int(self.d_k)).transpose(1, 2)
+        value = value.view(value.size(0), value.size(1), int(self.num_heads), int(self.d_k)).transpose(1, 2)
         
         attention = MultiHeadAttention.attention(query, key, value, self.dropout)
         
