f81a0e02f727f905d72920176e1c2e2ee89addb7
SponeKiller <124689362+SponeKiller@users.noreply.github.com>
2025-01-10 18:19:56 +0100

    debuging

--- model_i2t/train/train.py
+++ model_i2t/train/train.py
@@ -213,8 +213,12 @@ class Train():
             output = self.model.forward(batch['decoder_input'].to(self.device))
 
             # Compare the output with the label
-            label: torch.Tensor = batch['labels'].to(self.device, dtype=self.config.dtype) 
+            label: torch.Tensor = batch['labels'].to(self.device) 
 
+            print(f"LABEL{label}")
+            print(f"OUTPUT{output}")
+            print(f"LABEL DTYPE: {label.dtype}")
+            print(f"OUTPUT DTYPE: {output.dtype}")
             # Compute the loss using a simple cross entrophy
             loss: torch.Tensor = self.loss_fn(output, label)
 
