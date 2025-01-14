30ab8e704cbb8d639ed04f506d5625f177422dca
SponeKiller <124689362+SponeKiller@users.noreply.github.com>
2025-01-14 08:43:28 +0100

    debugging

--- model_i2t/train/train.py
+++ model_i2t/train/train.py
@@ -210,7 +210,7 @@ class Train():
             tqdm(train_ds, 
                  desc=f"Processing epoch {epoch:02d}")
         )
-        print(batch_iterator.shape)
+        print(train_ds.shape)
         for batch in batch_iterator:
             print(batch.shape)
             # Model prediction 
